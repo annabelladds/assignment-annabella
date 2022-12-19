ea250e7db363768c969d6013656d09189f2f7468
annabella dian dameria sinambela <hereannabella@gmail.com>
2022-12-19 20:04:26 +0700

    first commit

---
+++ src/Task.js
@@ -0,0 +1,5 @@
+const url = "YOURDEPLOYURL" // TODO: replace this
+const studentName = "ANNABELLA DIAN DAMERIA SINAMBELA" // TODO: replace this
+const studentId = "FE2966456" // TODO: replace this
+
+export { url, studentName, studentId }
